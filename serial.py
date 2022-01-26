f4bc42d6b82df984b7a6b90b24d0e1cb38151392
Christien Ng <ngchristien@gmail.com>
2022-01-26 11:45:30 -0800

    prompts answered

--- serial.py
+++ serial.py
@@ -19,3 +19,48 @@ class SerialGenerator:
     >>> serial.generate()
     100
     """
+
+    def __init__(self, start):
+        """initialize SerialGenerator with a start number"""
+        self.start = self.current_num = start
+
+    def __repr__(self):
+        """Show representation."""
+        return f"<SerialGenerator start={self.start} next={self.current_num}>"
+
+    def generate(self):
+        """print next sequential number"""
+        self.current_num += 1
+        return self.current_num - 1
+        
+    def reset(self):
+        """reset number back to the original start number"""
+        self.current_num = self.start
+
+
+################ ALTERNATE SOLUTION ########################################
+    # def __init__(self, start):
+    #     """initialize SerialGenerator with a start number"""
+    #     self.start = start
+
+    # def generate(self):
+    #     """
+    #     print the next sequential number
+    #     add each number to serialnums.txt file
+    #     """
+    #     output_file = open("serialnums.txt", "a")
+    #     output_file.write(f"{self.start}\n")
+    #     self.start += 1
+    #     output_file.close()
+    #     return self.start -1
+        
+    # def reset(self):
+    #     """
+    #     set self.start back to the original start number by grabbing first 3 chars in in line 1 of serialnums.txt
+    #     only works if original number is same number as the last output num
+    #     """
+    #     input_file = open("serialnums.txt", "r").readline()
+    #     self.start = int(input_file)
+    #     output_file = open("serialnums.txt", "w")
+    #     output_file.write(f"{self.start}")
+    #     output_file.close()
--- wordfinder.py
+++ wordfinder.py
@@ -1,5 +1,59 @@
 """Word Finder: finds random words from a dictionary."""
 
+import random
 
 class WordFinder:
-    ...
+    """Machine to get random word from list of words.
+
+    assume you have a file that looks like this:
+        cat
+        dog
+        porcupine
+
+    >>> wf = WordFinder("/Users/christien/github/Springboard-Assignments/18.4_python-oo-practice/words.txt")
+    5 words read
+
+    >>> wf.random()
+    ''
+
+    >>> wf.random()
+    'cat'
+
+    >>> wf.random()
+    'porcupine'
+    
+    >>> wf.random()
+    '# comment'
+    
+    >>> wf.random()
+    'dog'
+    """
+
+    def __init__(self, src):
+        """initialize SerialGenerator with a start number"""
+        file = open(src)
+        self.words = self.count_words(file)
+        print(f"{len(self.words)} words read")
+
+    def count_words(self, file):
+        """counts length of words in list"""
+        return [word.strip() for word in file]
+
+    def random(self):
+        """return random word"""
+        return random.choice(self.words)
+
+
+class SpecialWordFinder(WordFinder):
+    """Does not return empty lines or # comments
+
+    >>> swf = SpecialWordFinder("/Users/christien/github/Springboard-Assignments/18.4_python-oo-practice/words.txt")
+    3 words read
+
+    >>> swf.random()
+    'cat'
+    """
+
+    def count_words(self, file):
+        return [w.strip() for w in file
+        if w.strip() and not w.startswith("#")]
